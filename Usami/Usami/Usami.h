1e355d22eae808aa7957af68ee4ad1ba2c40bab4
史 翔新 <shi_xiangxin@mages.co.jp>
2016-10-27 14:23:58 +0900

    Initial Xcode project

---
+++ Usami/Usami/Usami.h
@@ -0,0 +1,19 @@
+//
+//  Usami.h
+//  Usami
+//
+//  Created by 史　翔新 on 2016/10/27.
+//  Copyright © 2016年 net.crazism. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Usami.
+FOUNDATION_EXPORT double UsamiVersionNumber;
+
+//! Project version string for Usami.
+FOUNDATION_EXPORT const unsigned char UsamiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Usami/PublicHeader.h>
+
+
