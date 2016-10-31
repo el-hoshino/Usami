1a9ff3cb2a14ef7407aa061397a61a4fa340c764
史 翔新 <shi_xiangxin@mages.co.jp>
2016-10-31 18:13:23 +0900

    Add some CIColor values

---
+++ Usami/Usami/CIColor.swift
@@ -0,0 +1,19 @@
+//
+//  CIColor.swift
+//  Usami
+//
+//  Created by 史　翔新 on 2016/10/31.
+//  Copyright © 2016年 net.crazism. All rights reserved.
+//
+
+import CoreImage
+
+extension CIColor {
+	
+	public static let clear = CIColor(red: 0, green: 0, blue: 0, alpha: 0)
+	
+	public static let white = CIColor(red: 1, green: 1, blue: 1)
+	
+	public static let black = CIColor(red: 0, green: 0, blue: 0)
+	
+}
