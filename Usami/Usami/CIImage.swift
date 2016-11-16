5771ab6ffc32d20077658a550336291cc4dc90ae
史 翔新 <shi_xiangxin@mages.co.jp>
2016-11-16 18:22:39 +0900

    Create RadialGradientGenerator filter

--- Usami/Usami/CIImage.swift
+++ Usami/Usami/CIImage.swift
@@ -7,6 +7,7 @@
 //
 
 import CoreImage
+import Eltaso
 
 extension CIImage {
 	
@@ -25,19 +26,18 @@ extension CIImage {
 	
 	public static func createRadialGradientImageWithExtent(_ extent: CGRect, circleCenter: CGPoint, innerRadius: CGFloat, innerColor: CIColor, outerRadius: CGFloat, outerColor: CIColor) -> CIImage? {
 		
-		guard let generator = CIFilter(name: "CIRadialGradient") else {
-			return nil
-		}
+		let generator = RadialGradientGenerator()
 		
-		let centerVector = CIVector(cgPoint: circleCenter)
-		generator.setValue(centerVector, forKey: kCIInputCenterKey)
-		generator.setValue(innerRadius, forKey: "inputRadius0")
-		generator.setValue(outerRadius, forKey: "inputRadius1")
-		generator.setValue(innerColor, forKey: "inputColor0")
-		generator.setValue(outerColor, forKey: "inputColor1")
-		let croppedImage = generator.outputImage?.cropping(to: extent)
+		generator.extent = extent
+		generator.radialCenter = CIVector(cgPoint: circleCenter)
+		generator.innerRadius = innerRadius
+		generator.innerColor = innerColor
+		generator.outerRadius = outerRadius
+		generator.outerColor = outerColor
+		let generatedImage = generator.outputImage
 		
-		return croppedImage
+		return generatedImage
+		
 		
 	}
 	
