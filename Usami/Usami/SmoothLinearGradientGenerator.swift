d66f4dbc29a8f1ebce286918094bc5e8a590ca87
史 翔新 <shi_xiangxin@mages.co.jp>
2017-02-15 21:15:03 +0900

    Update SmoothLinearGradientGenerator to conform CIFilter subclass considerations

--- Usami/Usami/SmoothLinearGradientGenerator.swift
+++ Usami/Usami/SmoothLinearGradientGenerator.swift
@@ -10,46 +10,51 @@ import CoreImage
 
 public class SmoothLinearGradientGenerator: CIFilter {
 	
-	private let _generator = CIFilter(name: "CISmoothLinearGradient")
+	private let _generator: CIFilter = {
+		guard let filter = CIFilter(name: "CISmoothLinearGradient") else {
+			fatalError("CISmoothLinearGradient filter not exist")
+		}
+		return filter
+	}()
 	
-	public var extent: CGRect? = nil
-	public var aPoint: CIVector = CIVector(x: 0, y: 0)
-	public var aColor: CIColor = CIColor(red: 1, green: 1, blue: 1, alpha: 1)
-	public var bPoint: CIVector = CIVector(x: 0, y: 0)
-	public var bColor: CIColor = CIColor(red: 0, green: 0, blue: 0, alpha: 0)
+	public var defaultExtent: CGRect { return .zero }
+	public var defaultAPoint: CIVector { return CIVector(x: 0, y: 0) }
+	public var defaultAColor: CIColor { return CIColor(red: 1, green: 1, blue: 1, alpha: 1) }
+	public var defaultBPoint: CIVector { return CIVector(x: 0, y: 0) }
+	public var defaultBColor: CIColor { return CIColor(red: 0, green: 0, blue: 0, alpha: 0) }
+	
+	public lazy var inputExtent: CGRect = self.defaultExtent
+	public lazy var inputAPoint: CIVector = self.defaultAPoint
+	public lazy var inputAColor: CIColor = self.defaultAColor
+	public lazy var inputBPoint: CIVector = self.defaultBPoint
+	public lazy var inputBColor: CIColor = self.defaultBColor
 	
 	override public func setDefaults() {
 		super.setDefaults()
-		self._generator?.setDefaults()
-		self.extent = nil
-		self.aPoint = CIVector(x: 0, y: 0)
-		self.aColor = CIColor(red: 1, green: 1, blue: 1, alpha: 1)
-		self.bPoint = CIVector(x: 0, y: 0)
-		self.bColor = CIColor(red: 0, green: 0, blue: 0, alpha: 0)
-	}
+		self._generator.setDefaults()
+		self.inputExtent = self.defaultExtent
+		self.inputAPoint = self.defaultAPoint
+		self.inputAColor = self.defaultAColor
+		self.inputBPoint = self.defaultBPoint
+		self.inputBColor = self.defaultBColor	}
 	
 	override public var outputImage: CIImage? {
 		
-		guard let generator = self._generator else {
-			return nil
-		}
-		
-		let aPoint = self.aPoint
-		let aColor = self.aColor
-		let bPoint = self.bPoint
-		let bColor = self.bColor
+		let generator = self._generator
+		let extent = self.inputExtent
+		let aPoint = self.inputAPoint
+		let aColor = self.inputAPoint
+		let bPoint = self.inputBPoint
+		let bColor = self.inputBColor
 		
 		generator.setValue(aPoint, forKey: "inputPoint0")
 		generator.setValue(aColor, forKey: "inputColor0")
 		generator.setValue(bPoint, forKey: "inputPoint1")
 		generator.setValue(bColor, forKey: "inputColor1")
 		
-		let outputImage = generator.outputImage
-		if let extent = self.extent {
-			return outputImage?.cropping(to: extent)
-		} else {
-			return outputImage
-		}
+		let generatedImage = generator.outputImage
+		let outputImage = generatedImage?.cropping(to: extent)
+		return outputImage
 		
 	}
 
